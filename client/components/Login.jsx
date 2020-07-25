c91e3d23f9f60a2de272b25a5c8d631b5e6f0a96
serenackuo <hello@serenakuo.com>
2020-07-25 14:26:19 -0400

    login debacle

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
+// import react, useState, useEffect
+// import react router objects
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>HI FROM LOGIN!</div>;
-  }
-}
+// require App component
 
-export default Login;
+// create form function
+// Custom hook for handling input boxes
+// saves us from creating onChange handlers for them individually
+// const useInput = init => {
+//   const [ value, setValue ] = useState(init);
+//   const onChange = e => {
+//     setValue(e.target.value);
+//   }
+//   // return the value with the onChange function instead of setValue function
+//   return [ value, onChange ];
+// }
+
+// pass in props, create CreateItem component
+// all the states and setStates utilizes the userInput('')
+
+const LogIn = () => {
+  return <div />;
+};
+// function to save item
+// check for errors
+// if all good, set state
+
+// post req to Item
+// catch errors
+
+// return interface
+// input fields
+
+// create buttons with onClick for save and cancel;
+
+export default LogIn;
