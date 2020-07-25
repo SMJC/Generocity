1f1c5ee34810c89aec048e6298ccd38d73ad4986
Michelle Holland <michelle@MacBook-Pro.local>
2020-07-25 14:43:55 -0700

    add handlechange in signup

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 class SignUp extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userEmail: '',
+      password: '',
+      userZip: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(e) { // add as onchange method
+    this.setState({ [e.target.name]: e.target.value})
+   }
 
-  
   render() { 
 
 
@@ -15,20 +23,20 @@ class SignUp extends Component {
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">Email</label>
-          <input type="email" class="form-control" id="inputEmail4" placeholder="Email"></input>
+          <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="userEmail" onChange={(e) => this.handleChange(e)}></input>
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Password</label>
-          <input type="password" class="form-control" id="inputPassword4" placeholder="Password"></input>
+          <input type="password" class="form-control" id="inputPassword4" placeholder="Password" name="password" onChange={(e) => this.handleChange(e)}></input>
         </div>
       </div>
       <div class="form-group">
         <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
+        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" name=""></input>
       </div>
       <div class="form-group">
         <label for="inputAddress2">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" ></input>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
@@ -44,18 +52,18 @@ class SignUp extends Component {
         </div>
         <div class="form-group col-md-2">
           <label for="inputZip">Zip</label>
-          <input type="text" class="form-control" id="inputZip"></input>
+          <input type="text" class="form-control" id="inputZip" name="userZip"></input>
         </div>
       </div>
       <div class="form-group">
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck"></input>
+          <input class="form-check-input" type="checkbox" id="gridCheck" ></input>
           <label class="form-check-label" for="gridCheck">
             Check me out
           </label>
         </div>
       </div>
-      <button type="submit" class="btn btn-primary">Sign in</button>
+      <button type="submit" class="btn btn-primary">Sign Up</button>
     </form>
 
      );
