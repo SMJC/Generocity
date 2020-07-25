d5d86367be61cdcbdc6d8c59e458abe047fccc21
serenackuo <hello@serenakuo.com>
2020-07-25 11:19:22 -0400

    pseudocode for components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
+// import react router, switch, links, etc
+// import react useState and useEffect
+// import various components
+
+// create stateful App component to include:
+/*
+loggedIn: false,
+      username: '',
+      userLocation:,
+      userItems: [], // store cards in this array after fetching from API
+      userEmail:,
+      */
+// React hooks equivalent of component did mount
+// Similar to componentDidMount and componentDidUpdate:
+// useEffect(() => {
+// fetch request
+// set items with new state
+// catch error
+// });
+
+// render
+// if loggedIn is false, the error message
+// else add items into state by mapping with query for location
+
+// return nav with link to and react router components:
+// home
+// user
+// create item
+// logout (set the state)
+// login (set the state)
 
 const App = () => {
   return <div />;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.resolve(__dirname, '../')));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
+// react router
+app.get('/user', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+app.get('/item/:id', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+app.get('/logIn', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+app.get('/logOut', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+app.get('/createItem', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+
+
 app.use('*', (req, res) => {
   res.status(404).send('Bad Request');
 });
