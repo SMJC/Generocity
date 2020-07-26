1ad4ffb17a33ca9c8b04063d3f2be958eb26dc8e
Catherine <cc2368@cornell.edu>
2020-07-26 02:04:53 -0700

    added all routes needed for mvp

--- server/database/postgres_create.sql
+++ server/database/postgres_create.sql
@@ -7,7 +7,9 @@ CREATE TABLE public.users (
   "password" varchar ( 50 ) NOT NULL,
   "points" integer,
   "address_id" bigint NOT NULL,
-  CONSTRAINT "users_pk" PRIMARY KEY ("_id")
+	CONSTRAINT "users_pk" PRIMARY KEY ("_id")
+) WITH (
+  OIDS=FALSE
 );
 
 CREATE TABLE public.address (
@@ -15,30 +17,35 @@ CREATE TABLE public.address (
   "zipcode" integer NOT NULL,
   "street" varchar NOT NULL,
   "city" varchar NOT NULL,
-  "state" varchar NOT NULL
+  "state" varchar NOT NULL,
+  CONSTRAINT "address_pk" PRIMARY KEY ("_id")
+) WITH (
+  OIDS=FALSE
 );
 
-
-  CREATE TABLE public.items (
+CREATE TABLE public.items (
   "_id" serial NOT NULL,
-  "title" varchar UNIQUE NOT NULL,
+  "title" varchar NOT NULL,
   "description" varchar (50) NOT NULL,
   "image" varchar (50) NOT NULL,
   "category" varchar NOT NULL,
   "status" BOOLEAN,
-  "user_id" bigint NOT NULL
+  "user_id" bigint NOT NULL,
+    CONSTRAINT "items_pk" PRIMARY KEY ("_id")
+) WITH (
+  OIDS=FALSE
 );
 
 
- ALTER TABLE public.users ADD CONSTRAINT "users_fk0" FOREIGN KEY ("address_id") REFERENCES public.address("_id");
+
+
+ ALTER TABLE public.users ADD FOREIGN KEY ("address_id") REFERENCES public.address("_id");
  ALTER TABLE public.items ADD CONSTRAINT "items_fk0" FOREIGN KEY ("user_id") REFERENCES public.users("_id");
 
-
  INSERT INTO public.address VALUES (1, 92663, '123 daisy lane', 'oc', 'california');
- INSERT INTO public.address VALUES (2, 92663, '123 daisy lane', 'los angeles', 'california');
- INSERT INTO public.address VALUES (3, 92663, '123 daisy lane', 'sb', 'california');
- INSERT INTO public.address VALUES (4, 92663, '123 daisy lane', 'nyc', 'ny');
-
+ INSERT INTO public.address VALUES (2, 92663, '123 orchid lane', 'los angeles', 'california');
+ INSERT INTO public.address VALUES (3, 92663, '123 lily lane', 'sb', 'california');
+ INSERT INTO public.address VALUES (4, 92663, '123 basil lane', 'nyc', 'ny');
 
  INSERT INTO public.users VALUES (1, 'cc2368@cornell.edu', 'Catherine', 'Chiu', 'helloworld', 500, 1);
  INSERT INTO public.users VALUES (2, 'jm@gmail.com', 'John', 'Madrigal', 'helloworld', 500, 2);
@@ -46,14 +53,11 @@ CREATE TABLE public.address (
  INSERT INTO public.users VALUES (4, 'sk@gmail.com', 'Serena', 'Kuo', 'helloworld',  500, 4);
 
  INSERT INTO public.items VALUES (1, 'fiddle leaf fig', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
- INSERT INTO public.items VALUES (2, 'fiddle leaf fig', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
- INSERT INTO public.items VALUES (3, 'fiddle leaf fig', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
- INSERT INTO public.items VALUES (4, 'fiddle leaf fig', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
+ INSERT INTO public.items VALUES (2, 'monstera leaf', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
+ INSERT INTO public.items VALUES (3, 'bamboo palm', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
+ INSERT INTO public.items VALUES (4, 'orchid', 'lovely green addition to your home', 'image.url', 'home goods', FALSE, 1);
 
-
-
-
-
-
-
+ select setval('public.users__id_seq', 5, false);
+ select setval('public.address__id_seq', 5, false);
+ select setval('public.items__id_seq', 5, false);
 
