36b87fd6926b6d972888cf5c9d05e81827d4ee99
Catherine <cc2368@cornell.edu>
2020-07-25 21:57:13 -0700

    fixing psql bug

--- server/server.js
+++ server/server.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 const PORT = 3000;
+const itemRouter = require('./routes/itemsRouter.js');
 
+// require dotenv to hide server uri
+require('dotenv').config();
+
+// Handle Parsing of Request Body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Handle Requests for Static Files
 app.use('/', express.static(path.resolve(__dirname, '../')));
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+
+// Define Route Handlers
+app.use('/item', itemRouter);
+
+// Get Home Route 
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../index.html')));
+
 app.get('/profile', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
 
+
+
+// Catch-All to handle unknown routes
 app.use('*', (req, res) => {
   res.status(404).send('Bad Request');
 });
 
+// Global Error Handler
 app.use((err, req, res, next) => {
   console.log('Global error handler: ', err);
   res.status(500).send(err);
 });
 
+// Start Server
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
