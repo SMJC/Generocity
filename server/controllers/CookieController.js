4a30e683cf140c08378298a7ce4e8f920ad0e666
Michelle Holland <michelle@MacBook-Pro.local>
2020-07-27 11:26:49 -0700

    comment out session-auth to prevent breaks - needs updating for SQL DB

--- server/controllers/CookieController.js
+++ server/controllers/CookieController.js
@@ -1,23 +1,23 @@
-const cookieController = {};
-const User = require('../models/userModel');
+// const cookieController = {};
+// const User = require('../models/userModel');
 
-cookieController.setSSIDCookie = (req, res, next) => {
-  // console.log("from setssid testing login");
-  const { email, password } = req.body;
-  console.log("email in setSSIDcookie", email);
-  User.findOne({email: email}, (err, data) => {
-    if (err) return next({log: "Invalid query in SSIDCookie - can't find username"});
-    // data._id is the mongoDB id of the user
-    console.log("DATA in setSSIDcookie",data);
-    res.cookie('ssid', data._id, {httpOnly: true})
-    // store user ID in res.locals.ssid
-    res.locals.ssid = data._id;
+// cookieController.setSSIDCookie = (req, res, next) => {
+//   // console.log("from setssid testing login");
+//   const { email, password } = req.body;
+//   console.log("email in setSSIDcookie", email);
+//   User.findOne({email: email}, (err, data) => {
+//     if (err) return next({log: "Invalid query in SSIDCookie - can't find username"});
+//     // data._id is the mongoDB id of the user
+//     console.log("DATA in setSSIDcookie",data);
+//     res.cookie('ssid', data._id, {httpOnly: true})
+//     // store user ID in res.locals.ssid
+//     res.locals.ssid = data._id;
 
     
-    // console.log("setSSID response headers", res);
-    return next(); 
-  })
+//     // console.log("setSSID response headers", res);
+//     return next(); 
+//   })
 
-}
+// }
 
-module.exports = cookieController;
+// module.exports = cookieController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const UserController = require('../controllers/UserController.js');
-
+const CookieController = require('../controllers/CookieController.js')
 const router = express.Router();
 
 
@@ -13,33 +13,33 @@ router.get('/:user_id', UserController.getUserItems, (req, res, next) => {
   res.status(200).json({ allItems: res.locals.items });
 });
 
-// POST request to add user
-router.post('/signup', UserController.createUser, CookieController.setSSIDCookie, SessionController.startSession,
-(req, res) => {
-   return res.status(200).json({isLoggedIn: true});
-})
-
-// handle login requests
-router.post('/login',   
-UserController.verifyUser,             
-CookieController.setSSIDCookie,             
-SessionController.startSession, 
-(req, res) => { 
-  return res.status(200).json({isLoggedIn: true})
-})
-
-// hanlde logout requests
-// router.post('/:user_id/logout', 
-//     SessionController.endSession, 
-//     (req, res) => {
-//       return res.status(200).json({})
+// // POST request to add user
+// router.post('/signup', UserController.createUser, CookieController.setSSIDCookie, SessionController.startSession,
+// (req, res) => {
+//    return res.status(200).json({isLoggedIn: true});
 // })
 
-// check for session on componentDidMount
-router.get('/checksession',
-  SessionController.isLoggedIn, (req, res) => {
-    // 200 response will provide client with user email
-    return res.status(200).json({email: res.locals.email});
-})
+// // handle login requests
+// router.post('/login',   
+// UserController.verifyUser,             
+// CookieController.setSSIDCookie,             
+// SessionController.startSession, 
+// (req, res) => { 
+//   return res.status(200).json({isLoggedIn: true})
+// })
+
+// // hanlde logout requests
+// // router.post('/:user_id/logout', 
+// //     SessionController.endSession, 
+// //     (req, res) => {
+// //       return res.status(200).json({})
+// // })
+
+// // check for session on componentDidMount
+// router.get('/checksession',
+//   SessionController.isLoggedIn, (req, res) => {
+//     // 200 response will provide client with user email
+//     return res.status(200).json({email: res.locals.email});
+// })
 
 module.exports = router;
