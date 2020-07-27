55faa73848c289c051417bbab1d866b94e8d4e4c
serenackuo <hello@serenakuo.com>
2020-07-26 20:57:48 -0400

    fix minor style issue

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,53 +1,68 @@
-
 import React, { Component } from 'react';
 import '../scss/app.scss';
 
 class Login extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-    }
+    super(props);
+    this.state = {};
   }
 
-
-
-render() {
-  return (
-    <div className="loginWrapper container">
-          <div class="row" style={{height: '15vh'}}></div>
-    <div class="col">
-    <h3 style={{textAlign: 'center', margin: '30px'}}>join the community</h3>
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="userEmail" onChange={(e) => this.props.handleChange(e)}></input>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" onChange={(e) => this.props.handleChange(e)}></input>
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary" onClick={(e) => this.props.handleLoginSubmit(e)}>Submit</button>
-</form>
-</div>
-<div class="row" style={{height: '20vh'}}></div>
-    </div>
-  )
-}
+  render() {
+    return (
+      <div className="loginWrapper container">
+        <div class="row" style={{ height: '15vh' }}></div>
+        <div class="col">
+          <h3 style={{ textAlign: 'center', margin: '30px' }}>join the community</h3>
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                name="userEmail"
+                onChange={(e) => this.props.handleChange(e)}
+              ></input>
+              <small id="emailHelp" class="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => this.props.handleChange(e)}
+              ></input>
+            </div>
+            <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck"></input>
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              onClick={(e) => this.props.handleLoginSubmit(e)}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div class="row" style={{ height: '20vh' }}></div>
+      </div>
+    );
+  }
 }
 export default Login;
-
-
-
-
-
-
-
-
 
 // import react, useState, useEffect
 // import react router objects
