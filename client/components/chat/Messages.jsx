07b547a43023b8ac16b4d8c6d8b741814a38c244
Michelle Holland <michelle@MacBook-Pro.local>
2020-07-27 16:08:52 -0700

    styling

--- client/components/chat/Messages.jsx
+++ client/components/chat/Messages.jsx
@@ -22,12 +22,7 @@ const Messages = (props) => {
     )
   })
   console.log(listOfRooms);
-  /* TODO */
-  // add a method on Home component to append to the userMessages ID when 'send message' button is clicked
-  // dynamically add buttons to the messages list for each socketID (these will be passed down as userMessengers prop from Home component)
-  // value of each button should be socketID
-  // buttons sould setMessenger to socketID
-  // pass down currentMessenger to Chat component
+
   return ( 
     <div className="container msgContainer mx-10vh">
       <div className="row" style={{height: '100vh', width: '100vh'}}>
