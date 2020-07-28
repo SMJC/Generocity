a9f3ae1ff442c56d67f6a02dad107f036161872b
Michelle Holland <michelle@MacBook-Pro.local>
2020-07-28 08:58:41 -0700

    add comments

--- server/server.js
+++ server/server.js
@@ -20,21 +20,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 io.on("connection", socket => {
-  console.log('new socket connection!', socket.id)
+  console.log('new socket connection!, socket.id: ', socket.id)
   socket.on('join', ({ name, room }, callback) => {
     // name is user's name, room is the other user's name
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
 
-    socket.join(user.room); // joins suer to room
+    socket.join(user.room); // joins user to current room
     socket.emit('message', { user: 'admin', text: `Hi, ${user.name}, you are now chatting with ${user.room}!` })
+    // broadcast methods emits event to everyone EXCEPT the user
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
   })
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-
+    // emit the message to the room and the room only
     io.to(user.room).emit('message', { user: user.name, text: message });
     // SEND ROOMDATA TO ROOM ON CONNECTION
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
@@ -49,12 +50,13 @@ io.on("connection", socket => {
     if (user) {
 
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      /* Room data event listener has not been defined in Chat!! */
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
     ;
   })
 })
-// from client, send message to scoket when room is created
+
 
 // Handle Requests for Static Files
 app.use('/', express.static(path.resolve(__dirname, '../')));
